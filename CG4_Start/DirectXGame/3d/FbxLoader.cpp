9fec8d62edf1372bed270795ff2db57294c0da2f
1327-Imai <97903480+1327-Imai@users.noreply.github.com>
2023-04-26 14:45:42 +0900

    資料01_07まで

--- CG4_Start/DirectXGame/3d/FbxLoader.cpp
+++ CG4_Start/DirectXGame/3d/FbxLoader.cpp
@@ -1,6 +1,8 @@
 ﻿#include "FbxLoader.h"
 #include <cassert>
 
+using namespace DirectX;
+
 const std::string FbxLoader::baseDirectory = "Resources/";
 
 FbxLoader* FbxLoader::GetInstance()
@@ -59,4 +61,61 @@ void FbxLoader::LoadModelFromFile(const string& modelName)
 	//ファイルからロードしたFBXの情報をシーンにインポート
 	fbxImporter->Import(fbxScene);
 
+	//モデル生成
+	Model* model = new Model;
+	model->name = modelName;
+
+	//FBXノードの数を取得
+	int nodeCount = fbxScene->GetNodeCount();
+	//あらかじめ必要部分のメモリを確保することでアドレスがずれるのを予防
+	model->nodes.reserve(nodeCount);
+
+	//ルートノードから順に解析してモデルに流し込む
+	ParseNodeRecursive(model , fbxScene->GetRootNode());
+
+	//FBXシーン開放
+	fbxScene->Destroy();
+
+}
+
+void FbxLoader::ParseNodeRecursive(Model* model , FbxNode* fbxNode , Model::Node* parent)
+{
+	//モデルにノードを追加
+	model->nodes.emplace_back();
+	Model::Node& node = model->nodes.back();
+	//ノード名を取得
+	string name = fbxNode->GetName();
+
+	//FBXノードのローカル移動情報
+	FbxDouble3 rotation = fbxNode->LclRotation.Get();
+	FbxDouble3 scaling = fbxNode->LclScaling.Get();
+	FbxDouble3 translation = fbxNode->LclTranslation.Get();
+
+	//解析変換して代入
+	node.rotation = {(float)rotation[0] , (float)rotation[1] , (float)rotation[2] , 0.0f};
+	node.scaling = {(float)scaling[0] , (float)scaling[1] , (float)scaling[2] , 0.0f};
+	node.translation = {(float)translation[0] , (float)translation[1] , (float)translation[2] , 1.0f};
+
+	//回転角を度数法から弧度法に変換
+	node.rotation.m128_f32[0] = XMConvertToRadians(node.rotation.m128_f32[0]);
+	node.rotation.m128_f32[1] = XMConvertToRadians(node.rotation.m128_f32[1]);
+	node.rotation.m128_f32[2] = XMConvertToRadians(node.rotation.m128_f32[2]);
+
+	//スケール、回転、平行移動行列の計算
+	XMMATRIX matScaling , matRotation , matTransration;
+	matScaling = XMMatrixScalingFromVector(node.scaling);
+	matRotation = XMMatrixRotationRollPitchYawFromVector(node.rotation);
+	matTransration = XMMatrixTranslationFromVector(node.translation);
+
+	//1ローカル変形行列の計算
+	node.transform = XMMatrixIdentity();
+	node.transform *= matScaling;
+	node.transform *= matRotation;
+	node.transform *= matTransration;
+
+	//子ノードに対して再起呼び出し
+	for (int i = 0; i < fbxNode->GetChildCount(); i++)
+	{
+		ParseNodeRecursive(model , fbxNode->GetChild(i),&node);
+	}
 }
