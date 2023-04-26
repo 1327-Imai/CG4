288d328063cf6683f0849426079dc6fb41483743
1327-Imai <97903480+1327-Imai@users.noreply.github.com>
2023-04-26 14:08:49 +0900

    資料01_06まで

---
+++ CG4_Start/DirectXGame/3d/FbxLoader.h
@@ -0,0 +1,62 @@
+﻿#pragma once
+
+#include "fbxsdk.h"
+#include <string>
+
+#include <d3d12.h>
+#include <d3dx12.h>
+
+class FbxLoader
+{
+private://エイリアス
+	//stdを省略
+	using string = std::string;
+
+public://定数
+	//モデル格納ルートパス
+	static const string baseDirectory;
+
+public:
+	/// <summary>
+	/// シングルトンインスタンスの取得
+	/// </summary>
+	/// <returns>インスタンス</returns>
+	static FbxLoader* GetInstance();
+
+	/// <summary>
+	/// 初期化処理
+	/// </summary>
+	/// <param name="device"></param>
+	void Initialize(ID3D12Device* device);
+
+	/// <summary>
+	/// 終了処理
+	/// </summary>
+	void Finalize();
+
+	/// <summary>
+	/// ファイルからFBXモデル読み込み
+	/// </summary>
+	/// <param name="modelName"></param>
+	void LoadModelFromFile(const string& modelName);
+
+private://メンバ変数
+	//デバイス
+	ID3D12Device* device = nullptr;
+
+	//FBXマネージャ
+	FbxManager* fbxManager = nullptr;
+
+	//FBXインポータ
+	FbxImporter* fbxImporter = nullptr;
+
+private:
+	// privateなコンストラクタ（シングルトンパターン）
+	FbxLoader() = default;
+	// privateなデストラクタ（シングルトンパターン）
+	~FbxLoader() = default;
+	// コピーコンストラクタを禁止（シングルトンパターン）
+	FbxLoader(const FbxLoader& obj) = delete;
+	// コピー代入演算子を禁止（シングルトンパターン）
+	void operator=(const FbxLoader& obj) = delete;
+};
